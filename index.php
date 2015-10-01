2dbfaaa501e66770b82ab176267960614831ff4e
Chyngyz Dyikanbaev <chipalino@live.com>
2015-10-01 17:57:31 +0600

    Update index.php

--- index.php
+++ index.php
@@ -3,5 +3,5 @@ require_once 'auth.php';
 $username = $_POST['username']; 
 $password = $_POST['password'];
 $status = user_login_ldap($username, $password);
-// To be Continued
+echo '{"status":"$status"}'
  ?>
